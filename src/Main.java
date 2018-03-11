7a790403c8ada8e19c5a31f42240367e0b346adf
aniskhan <anisa@khandkar.net>
2018-03-11 11:42:15 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+/*
+Write a program that given a two dimensional array of characters prints 
+out the combination of every letter with every other letter. For eg, if 
+you had:
+
+{{a, b},{c, d},{e, f}}
+
+Your output would be:
+ace
+acf
+ade
+adf
+bce
+bcf
+bde
+bdf
+
+NOTE: 
+1) Your program should be able to handly ANY two dimensional character 
+array.
+
+2) Get the 2D array at runtime. Either from the user or a file. To make 
+your life easy, your file can have the following format:
+
+3	//how many rows your arr[][] will have.
+4	//length of your arr[0]
+a b c d	//values of arr[0]
+2	//length of your arr[1]
+e f	//values of arr[1]
+3	//length of your arr[2]
+g h i	//values of arr[2]
+
+The comments are there just to explain what each line means. Don't put 
+them in your data file.  This format will help you create your jagged 
+arrays because it tells you the sizes of each one of them.
+
+3) Do not use recursion.
+
+
+Fun fact! This was a real interview question.*/
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
